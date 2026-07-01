476804c5416eb048379c8818ef0e021ec26db3ed
Pablo Salazar <salazarpabloale@gmail.com>
2026-07-01 11:38:30 -0600

    Adding Header and quiz

---
+++ section12/src/components/Quiz.jsx
@@ -0,0 +1,30 @@
+import { useState } from "react"
+import QUESTIONS from '../questions'
+
+
+export default function Quiz() {
+
+    const [userAnswers, setUserAnswers] = useState([])
+    const activeQuestionIndex = userAnswers.length
+
+    function handleSelectedAnswer(selectedAnswer) {
+        console.log('Answer Selected')
+        setUserAnswers(prevValue => [...prevValue, selectedAnswer])
+    }
+
+    return (
+        <div id="quiz">
+            <div id="question">
+                <h2> {QUESTIONS[activeQuestionIndex].text}</h2>
+
+                <ul id="answers">
+                    {QUESTIONS[activeQuestionIndex].answers.map(answer => (
+                        <li key={answer} className="answer">
+                            <button onClick={() => handleSelectedAnswer(answer)}>{answer}</button>
+                        </li>
+                    ))}
+                </ul>
+            </div>
+        </div>
+    )
+}
