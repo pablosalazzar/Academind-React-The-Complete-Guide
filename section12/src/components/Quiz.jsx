a41a14aa855a0f8624cfbcde1a9fc9150912263f
Pablo Salazar <salazarpabloale@gmail.com>
2026-07-13 23:05:08 -0600

    Adding results to page

--- section12/src/components/Quiz.jsx
+++ section12/src/components/Quiz.jsx
@@ -4,6 +4,7 @@ import quizCompletedImg from '../assets/quiz-complete.png'
 import QuestionTimer from "./QuestionTimer"
 import Answers from "./Answers"
 import Question from "./Question"
+import Summary from './Summary'
 
 
 export default function Quiz() {
@@ -25,53 +26,9 @@ export default function Quiz() {
     const handleTimeExpire = useCallback(() => handleSelectedAnswer(null), [handleSelectedAnswer])
 
     if (quizFinish) {
-        let skip = 0
-        let correct = 0
-        let incorrect = 0
-
-        let wrongAnswers = new Map()
-        // loop the results and start calculating thr esutl s
-        for (const [index, answer] of userAnswers.entries()) {
-
-            if (answer == null) {
-                skip++
-            } else {
-                // verify if this answer its correct or incorrect
-                if (answer === QUESTIONS[index].answers[0]) {
-                    correct++
-                } else {
-                    incorrect++
-                    wrongAnswers.set(index, answer)
-                }
-
-            }
-        }
 
         return (
-            <div id="summary">
-                <img src={quizCompletedImg} alt="Trophy icon" />
-                <h2>Quiz Completed!</h2>
-
-                {/* // I wnat to show here the data  */}
-                Total Of skip questions = {skip}
-                <br />
-
-                {/* How many skip question I have  */}
-                Total of good quesitons = {correct}
-                <br />
-
-                {/* Hiow many worng questions i have */}
-                Total of wrong questions = {incorrect}
-                <br />
-                <br />
-
-                {[...wrongAnswers].map(([questionIndex, wrongAnswer]) => (
-                    <div key={questionIndex}>
-                        Question {questionIndex}: {wrongAnswer}
-                    </div>
-                ))}
-
-            </div>
+            <Summary userAnswers={userAnswers}/>
         )
     }
 
